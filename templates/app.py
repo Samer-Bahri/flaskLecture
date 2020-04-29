e950f07293e27c4221c9fb494d76b1ed5138eddc
SamerBahri <samerbahri@gmail.com>
2020-04-29 06:07:46 -0700

    Conditioned Flask routes

--- templates/app.py
+++ templates/app.py
@@ -1,7 +1,11 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template("index.html", new_year = new_year)
