77a8d9dd270390acd9c9a0044f84bab927216e7a
SamerBahri <samerbahri@gmail.com>
2020-04-28 18:28:03 -0700

    Created set and square function

--- hello.py
+++ hello.py
@@ -10,3 +10,16 @@ else:
 names = ["Alice", "Bob", "Charlie"]
 for name in names:
     print(name)
+
+s = set()
+s.add(1)
+s.add(3)
+s.add(5)
+s.add(3)
+print(s)
+
+def square(x):
+    return x * x
+
+for i in range(10):
+    print("{} squared is {}".format(i, square(i)))
