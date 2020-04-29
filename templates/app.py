c4eda6d4ae8bb962e6cd60d7a2999f6391a70bb9
SamerBahri <samerbahri@gmail.com>
2020-04-29 06:45:37 -0700

    Post method test

--- templates/app.py
+++ templates/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/more")
-def more():
-    return render_template("more.html")
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("name")
+    return render_template("hello.html", name=name)
