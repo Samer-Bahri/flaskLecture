96a1064443b183b185acc78a611a9ae682b95a50
SamerBahri <samerbahri@gmail.com>
2020-04-29 06:21:38 -0700

    Configured app file for template compatibility

--- templates/app.py
+++ templates/app.py
@@ -1,11 +1,11 @@
-import datetime
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    now = datetime.datetime.now()
-    new_year = now.month == 1 and now.day == 1
-    return render_template("index.html", new_year = new_year)
+    return render_template("index.html")
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
